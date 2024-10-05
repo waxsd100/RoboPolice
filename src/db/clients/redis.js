d2b417c9450b06659b47da231cadf65afe5ae002
Colton B <main@coltvel.com>
2024-10-04 22:08:50 -0400

    whoopsie, fixed redis

--- src/db/clients/redis.js
+++ src/db/clients/redis.js
@@ -1,7 +1,8 @@
 const Redis = require('ioredis')
 const redis = new Redis({
   host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379
+  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+  password: process.env.REDIS_PASSWORD
 })
 
 module.exports = redis
