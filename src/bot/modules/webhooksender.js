77cdc2ed4def1e7841d9b2d308878c4cc24956d5
Colton B <main@coltvel.com>
2024-10-05 19:41:07 -0400

    test

--- src/bot/modules/webhooksender.js
+++ src/bot/modules/webhooksender.js
@@ -33,6 +33,8 @@ module.exports = async pkg => {
     await cacheGuild(pkg.guildID)
     return
   }
+  console.log(guildSettings.getEventByName(pkg.eventName))
+  console.log(pkg.eventName)
   if (!guildSettings.getEventByName(pkg.eventName)) return
   if (!global.bot.getChannel(guildSettings.getEventByName(pkg.eventName))) {
     await global.redis.del(`webhook-${guildSettings.getEventByName(pkg.eventName)}`)
@@ -41,8 +43,7 @@ module.exports = async pkg => {
   }
   if (!global.bot.getChannel(guildSettings.getEventByName(pkg.eventName))?.permissionsOf(global.bot.user.id).json.manageWebhooks || !global.bot.getChannel(guildSettings.getEventByName(pkg.eventName)).permissionsOf(global.bot.user.id).json.viewAuditLog) return
   const webhook = await webhookCache.getWebhook(guildSettings.getEventByName(pkg.eventName))
-  console.log(guildSettings.getEventByName(pkg.eventName))
-  console.log(pkg.eventName)
+  console.log(pkg.eventName + " - passed")
   let webhookID, webhookToken
   if (webhook) {
     const split = webhook.split('|')
