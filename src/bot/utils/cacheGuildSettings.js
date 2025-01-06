bbce1333f569ce1856e517a52041db120c63435d
sdmway <vladyslav.p.e@gmail.com>
2025-01-06 14:43:02 +0100

    update

--- src/bot/utils/cacheGuildSettings.js
+++ src/bot/utils/cacheGuildSettings.js
@@ -1,9 +1,9 @@
-const getAllGuilds = require('../../db/interfaces/postgres/read').getAllGuilds
-const GuildSettings = require('../bases/GuildSettings')
+const getAllGuilds = require('../../db/interfaces/postgres/read').getAllGuilds;
+const GuildSettings = require('../bases/GuildSettings');
 
 module.exports = async () => {
-  const allDBGuilds = await getAllGuilds()
-  allDBGuilds.forEach(guild => {
-    global.bot.guildSettingsCache[guild.id] = new GuildSettings(guild)
-  })
-}
+	const allDBGuilds = await getAllGuilds();
+	allDBGuilds.forEach((guild) => {
+		global.bot.guildSettingsCache[guild.id] = new GuildSettings(guild);
+	});
+};
