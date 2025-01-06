06191fc60b156b8c4003b7163d0a3aa01812dcb4
sdmway <vladyslav.p.e@gmail.com>
2025-01-06 14:34:20 +0100

    Update redis.js

--- src/db/clients/redis.js
+++ src/db/clients/redis.js
@@ -1,7 +1,8 @@
-const Redis = require('ioredis')
+const Redis = require('ioredis');
 const redis = new Redis({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379
-})
+	host: process.env.REDIS_HOST || 'localhost',
+	port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+	db: process.env.REDIS_DB ? parseInt(process.env.REDIS_DB) : 2,
+});
 
-module.exports = redis
+module.exports = redis;
