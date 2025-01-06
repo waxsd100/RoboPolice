571d49d5ff4bd6b01601dee590af45189a8a87dc
sdmway <vladyslav.p.e@gmail.com>
2025-01-06 14:33:13 +0100

    Update commandIndexer.js

--- src/miscellaneous/commandIndexer.js
+++ src/miscellaneous/commandIndexer.js
@@ -1,14 +1,13 @@
-const fs = require('fs')
-const path = require('path')
-const GenericCommand = require('../bot/bases/GenericCommand')
+const fs = require('fs');
+const path = require('path');
+const GenericCommand = require('../bot/bases/GenericCommand');
 
 module.exports = () => {
-  const files = fs.readdirSync(path.resolve('src', 'bot', 'commands'))
-  files.forEach(filename => {
-    if (require.cache[path.resolve('src', 'bot', 'commands', filename)]) {
-      delete require.cache[path.resolve('src', 'bot', 'commands', filename)]
-    }
-    // truly gross code that should be remade eventually
-    new GenericCommand(require(path.resolve('src', 'bot', 'commands', filename)))
-  })
-}
+	const files = fs.readdirSync(path.resolve('src', 'bot', 'commands'));
+	files.forEach((filename) => {
+		if (require.cache[path.resolve('src', 'bot', 'commands', filename)]) {
+			delete require.cache[path.resolve('src', 'bot', 'commands', filename)];
+		}
+		new GenericCommand(require(path.resolve('src', 'bot', 'commands', filename)));
+	});
+};
